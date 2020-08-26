8f91c6812f58f01ec8acde9b3d1ee5ffeb3d3e43
Husayn Hakeem <husaynhakeem@gmail.com>
2020-08-25 22:15:59 -0700

    Map UI layout authentication options to methods in MainActivity

--- BiometricSample/app/src/main/java/com/husaynhakeem/biometricsample/MainActivity.kt
+++ BiometricSample/app/src/main/java/com/husaynhakeem/biometricsample/MainActivity.kt
@@ -1,57 +1,93 @@
 package com.husaynhakeem.biometricsample
 
+import android.hardware.biometrics.BiometricManager.Authenticators.*
 import android.os.Bundle
+import android.security.keystore.KeyProperties.AUTH_BIOMETRIC_STRONG
+import android.security.keystore.KeyProperties.AUTH_DEVICE_CREDENTIAL
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
-import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.layout_authentication_confirmation.*
+import kotlinx.android.synthetic.main.layout_authenticator_types.*
+import kotlinx.android.synthetic.main.layout_configuration_change.*
+import kotlinx.android.synthetic.main.layout_negative_button.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        // BiometricManager to check whether we can authenticate
+    private fun getAuthenticationTypes(): Int {
+        val strong = if (authenticatorStrong.isChecked) BIOMETRIC_STRONG else 0
+        val weak = if (authenticatorWeak.isChecked) BIOMETRIC_WEAK else 0
+        val deviceCredential = if (authenticatorDeviceCredential.isChecked) DEVICE_CREDENTIAL else 0
+        return strong or weak or deviceCredential
+    }
+
+    private fun showNegativeButton(): Boolean {
+        return negativeButton.isChecked
+    }
+
+    private fun showAuthenticationConfirmation(): Boolean {
+        return authenticationConfirmation.isChecked
+    }
+
+    private fun keepAuthenticationDialogOnConfigurationChange(): Boolean {
+        return configurationChange.isChecked
+    }
+
+    private fun getSecretKeyType(): Int {
+        val strong = if (authenticatorStrong.isChecked) AUTH_BIOMETRIC_STRONG else 0
+        val deviceCredential =
+            if (authenticatorDeviceCredential.isChecked) AUTH_DEVICE_CREDENTIAL else 0
+        return strong or deviceCredential
+    }
+
+    private fun canAuthentication() {
         val biometricManager = BiometricManager.from(this)
-        val canAuthenticate = biometricManager.canAuthenticate()
-        Log.d(TAG, "Can authenticate: $canAuthenticate")
+        val canAuthenticate: BiometricError = biometricManager.canAuthenticate()
+        log(canAuthenticate.literal())
     }
 
     private fun authenticate() {
         // BiometricPrompt to launch an authentication operation
         val biometricPrompt = BiometricPrompt(
-                this, // FragmentActivity
-                ContextCompat.getMainExecutor(this),
-                object : BiometricPrompt.AuthenticationCallback() {
-                    override fun onAuthenticationSucceeded(
-                            result: BiometricPrompt.AuthenticationResult) {
-                        Log.d(TAG, "Authentication succeeded")
-                    }
-
-                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                        Log.d(TAG, "Authentication errored out - Error code $errorCode - $errString")
-                    }
-
-                    override fun onAuthenticationFailed() {
-                        Log.d(TAG, "Authentication failed")
-                    }
+            this, // FragmentActivity
+            ContextCompat.getMainExecutor(this),
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    log("Authentication succeeded")
                 }
+
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    log("Authentication errored out - Error code $errorCode - $errString")
+                }
+
+                override fun onAuthenticationFailed() {
+                    log("Authentication failed")
+                }
+            }
         )
 
         // BiometricPrompt.Info to define the properties of the dialog
         val biometricInfo = BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Title")
-                .setSubtitle("Subtitle")
-                .setDescription("Description")
-                .setNegativeButtonText("Negative text")
-                .setConfirmationRequired(false)
-                .setDeviceCredentialAllowed(true)
-                .build()
+            .setTitle("Title")
+            .setSubtitle("Subtitle")
+            .setDescription("Description")
+            .setNegativeButtonText("Negative text")
+            .setConfirmationRequired(false)
+            .setDeviceCredentialAllowed(true)
+            .build()
 
         biometricPrompt.authenticate(biometricInfo)
+    }
+
+    private fun log(message: String) {
+        Log.d(TAG, message)
     }
 
     companion object {
