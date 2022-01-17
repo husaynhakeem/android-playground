756ccddc9d818c01793669c1cbf73042bdaa5a1c
Husayn Hakeem <husaynhakeem@gmail.com>
2022-01-17 11:47:15 -0800

    Update Glance sample launch Activity's UI

--- GlanceSample/app/src/main/java/com/husaynhakeem/glancesample/MainActivity.kt
+++ GlanceSample/app/src/main/java/com/husaynhakeem/glancesample/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.husaynhakeem.glancesample.ui.theme.GlanceSampleTheme
 import kotlinx.coroutines.flow.launchIn
@@ -30,8 +36,14 @@ class MainActivity : AppCompatActivity() {
     private fun updateWidgets(widgets: List<Widget>) {
         setContent {
             GlanceSampleTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    WidgetInfo(widgets = widgets)
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = getString(R.string.app_name)) }
+                        )
+                    }
+                ) {
+                    WidgetInfo(modifier = Modifier.padding(it), widgets = widgets)
                 }
             }
         }
@@ -39,10 +51,37 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun WidgetInfo(widgets: List<Widget>) {
-    LazyColumn {
+fun WidgetInfo(
+    modifier: Modifier = Modifier,
+    widgets: List<Widget>
+) {
+    LazyColumn(modifier = modifier) {
         items(widgets) { widget ->
-            Text(text = widget.className)
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(text = widget.className, textDecoration = TextDecoration.Underline)
+                Box(modifier = Modifier.padding(start = 8.dp)) {
+                    if (widget.metadata.isEmpty()) {
+                        WidgetNoMetadata()
+                    } else {
+                        WidgetMetadata(widget.metadata)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WidgetNoMetadata() {
+    Text(text = "No metadata")
+}
+
+@Composable
+fun WidgetMetadata(metadata: List<WidgetMetadata>) {
+    Column {
+        metadata.forEach {
+            Text(text = it.id.toString())
+            Text(text = it.sizes.toString())
         }
     }
 }
