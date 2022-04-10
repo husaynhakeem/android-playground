35122b80d8cf57746e842fa87111223ff1c007e6
Husayn Hakeem <husaynhakeem@gmail.com>
2022-04-09 22:04:07 -0700

    Check if split is supported on device

--- ActivityEmbeddingSample/app/src/main/java/com/husaynhakeem/activityembeddingsample/ActivityEmbeddingSampleApplication.kt
+++ ActivityEmbeddingSample/app/src/main/java/com/husaynhakeem/activityembeddingsample/ActivityEmbeddingSampleApplication.kt
@@ -1,14 +1,20 @@
 package com.husaynhakeem.activityembeddingsample
 
 import android.app.Application
+import android.widget.Toast
 import androidx.window.core.ExperimentalWindowApi
 import androidx.window.embedding.SplitController
 
-class ActivityEmbeddingSampleApplication: Application() {
+class ActivityEmbeddingSampleApplication : Application() {
 
     @OptIn(ExperimentalWindowApi::class)
     override fun onCreate() {
         super.onCreate()
         SplitController.initialize(this, R.xml.split_configuration)
+
+        val splitController = SplitController.getInstance()
+        if (!splitController.isSplitSupported()) {
+            Toast.makeText(this, R.string.split_not_supported, Toast.LENGTH_LONG).show()
+        }
     }
 }
